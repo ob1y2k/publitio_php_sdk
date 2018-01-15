b65911c51c0bc82c268945fead867b65659eb367
ob1y2k <ob1y2k@users.noreply.github.com>
2018-01-15 09:20:59 +0100

    code update

--- examples.php
+++ examples.php
@@ -15,7 +15,7 @@ $response = $publitio_api->call("/files/list", "GET", array('offset' => '0', 'li
 #$response = $publitio_api->upload_file("samples/monkey.jpg", "file"); 
 
 // create (upload) local file, all options
-#$response = $publitio_api->upload_file("samples/monkey.jpg", "file", array('title' => 'zzz', 'description' => 'yyyyyy', 'tags' => 'tag1 tag2', 'privacy' => '1', 'option_download' => '1', 'option_embed' => '1', 'ad' => '1', 'wm' => 'qDaq')); 
+#$response = $publitio_api->upload_file("samples/monkey.jpg", "file", array('title' => 'zzz', 'description' => 'yyyyyy', 'tags' => 'tag1 tag2', 'privacy' => '1', 'option_download' => '1', 'option_ad' => '1')); 
 
 // create (remote upload) multiple files
 #$response = $publitio_api->call("/files/create/multi", "POST", array('files_count' => '3', 'file0url' => 'http://keepdsmile.com/publitio/espreso/rasa1.jpg', 'file1url' => 'http://keepdsmile.com/publitio/espreso/rasa2.jpg', 'file2url' => 'http://keepdsmile.com/publitio/espreso/rasa3.jpg')); 
@@ -24,16 +24,13 @@ $response = $publitio_api->call("/files/list", "GET", array('offset' => '0', 'li
 #$response = $publitio_api->call("/files/show/23dyM408", "GET"); 
 
 // update file with id 2Hjr3323
-#$response = $publitio_api->call("/files/update/2Hjr3323", "PUT", array('title' => 'xxxx', 'description' => 'wwww', 'tags' => 'tag1 tag2', 'privacy' => '1', 'option_download' => '1', 'option_embed' => '1', 'ad' => '1', 'wm' => 'qDaq')); // id
+#$response = $publitio_api->call("/files/update/2Hjr3323", "PUT", array('title' => 'xxxx', 'description' => 'wwww', 'tags' => 'tag1 tag2', 'privacy' => '1', 'option_download' => '1', 'option_ad' => '1')); // id
 
 // delete file with id 2Hjr3323
 #$response = $publitio_api->call("/files/delete/2Hjr3323", "DELETE"); 
 
 // get player for file with id 23dyM408
-#$response = $publitio_api->call("/files/player/23dyM408", "GET", array('player' => 'id', 'ad_tag' => 'id')); 
-
-// get players for multiple files in one call
-#$response = $publitio_api->call("/files/players", "GET", array('player' => 'id', 'ad_tag' => 'id', 'auto_play' => '2', 'files_count' => '3', 'file0id' => '23dyM408', 'file1id' => '9oMwgG8u', 'file2id' => '4RZxkP4G', 'file2player' => 'id2', 'file2ad_tag' => 'id3', 'file2auto_play' => '1')); 
+#$response = $publitio_api->call("/files/player/23dyM408", "GET", array('player' => 'id', 'adtag' => 'id')); 
 
 /*_________________________*/
 /* Player Class             */
@@ -42,34 +39,34 @@ $response = $publitio_api->call("/files/list", "GET", array('offset' => '0', 'li
 #$response = $publitio_api->call("/players/list", "GET");
 
 // create player
-#$response = $publitio_api->call("/players/create", "POST", array('name' => 'fliiby2', 'ad_tag_id' => 'myoverlay')); 
+#$response = $publitio_api->call("/players/create", "POST", array('name' => 'fliiby2', 'adtag_id' => 'myoverlay')); 
 
-// show player with id fliiby
-#$response = $publitio_api->call("/players/show/fliiby2", "GET"); 
+// show player with id myplayer
+#$response = $publitio_api->call("/players/show/myplayer", "GET"); 
 
-// update player with id fliiby2
-#$response = $publitio_api->call("/players/update/fliiby2", "PUT", array('ad_tag_id' => 'mypreroll', 'auto_play' => '1', 'skin' => '', 'geo_in' => '', 'geo_ex' => '')); // id
+// update player with id myplayer
+#$response = $publitio_api->call("/players/update/myplayer", "PUT", array('adtag_id' => 'mypreroll', 'auto_play' => '1', 'skin' => '')); // id
 
-// delete player with id fliiby2
-#$response = $publitio_api->call("/players/delete/fliiby2", "DELETE"); 
+// delete player with id myplayer
+#$response = $publitio_api->call("/players/delete/myplayer", "DELETE"); 
 
 /*_______________________________*/
 /* Advertising (Ad Tags) Class   */
 
 // list adtags
-#$response = $publitio_api->call("/advertising/list", "GET");
+#$response = $publitio_api->call("/players/adtags/list", "GET");
 
 // create adtag
-#$response = $publitio_api->call("/advertising/create", "POST", array('name' => 'mytestad', 'tag' => 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=')); 
+#$response = $publitio_api->call("/players/adtags/create", "POST", array('name' => 'mytestad', 'tag' => 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=')); 
 
 // show adtag with id mytestad
-#$response = $publitio_api->call("/advertising/show/mytestad", "GET"); 
+#$response = $publitio_api->call("/players/adtags/show/mytestad", "GET"); 
 
 // update adtag with id mytestad
-#$response = $publitio_api->call("/advertising/update/mytestad", "PUT", array('tag' => 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=', 'geo_in' => '', 'geo_ex' => '')); // id
+#$response = $publitio_api->call("/players/adtags/update/mytestad", "PUT", array('tag' => 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=')); // id
 
 // delete adtag with id mytestad
-#$response = $publitio_api->call("/advertising/delete/mytestad", "DELETE"); 
+#$response = $publitio_api->call("/players/adtags/delete/mytestad", "DELETE"); 
 
 /*_________________________*/
 /* File Versions Class   */
@@ -77,14 +74,14 @@ $response = $publitio_api->call("/files/list", "GET", array('offset' => '0', 'li
 // list versions for file with id 9oMwgG8u
 #$response = $publitio_api->call("/files/versions/list/9oMwgG8u", "GET");
 
-// create new version from file with id 9oMwgG8u, with options width=320, height=240, crop=fill, watermark=qDaq
-#$response = $publitio_api->call("/files/versions/create/9oMwgG8u", "POST", array('output_format' => 'mp4', 'options' => 'w_320,h_240,c_fill,wm_qDaq')); 
+// create new version from file with id 9oMwgG8u, with options width=320, height=240, crop=fill
+#$response = $publitio_api->call("/files/versions/create/9oMwgG8u", "POST", array('output_format' => 'mp4', 'options' => 'w_320,h_240')); 
 
 // show file version with id 1SMlAQpS
 #$response = $publitio_api->call("/files/versions/show/1SMlAQpS", "GET"); 
 
 // update file version with id 1SMlAQpS
-#$response = $publitio_api->call("/files/versions/update/1SMlAQpS", "PUT", array('privacy' => '1', 'protected' => '0')); 
+#$response = $publitio_api->call("/files/versions/update/1SMlAQpS", "PUT", array()); 
 
 // delete file version with id 1SMlAQpS
 #$response = $publitio_api->call("/files/versions/delete/1SMlAQpS", "DELETE"); 
@@ -98,14 +95,14 @@ $response = $publitio_api->call("/files/list", "GET", array('offset' => '0', 'li
 // create (upload) watermark from local file
 #$response = $publitio_api->upload_file("samples/smile.png", "watermark", array('name' => 'mytestwm', 'position' => 'bottom-right', 'padding' => '20')); 
 
-// show watermark with id qDaq
-#$response = $publitio_api->call("/watermarks/show/qDaq", "GET"); 
+// show watermark with id mytestwm
+#$response = $publitio_api->call("/watermarks/show/mytestwm", "GET"); 
 
-// update watermark with id qDaq
-#$response = $publitio_api->call("/watermarks/update/qDaq", "PUT", array('name' => 'cutenamewm', 'position' => 'top-right', 'padding' => '10')); 
+// update watermark with id mytestwm
+#$response = $publitio_api->call("/watermarks/update/mytestwm", "PUT", array('position' => 'top-right', 'padding' => '10')); 
 
-// delete watermark with id qhVu
-#$response = $publitio_api->call("/watermarks/delete/2gav", "DELETE"); 
+// delete watermark with id mytestwm
+#$response = $publitio_api->call("/watermarks/delete/mytestwm", "DELETE"); 
 
 /*_________________________*/
 
